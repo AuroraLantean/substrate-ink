35c6453805ea0ddb846a5ec42fab9e7a17424411
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-10-13 19:53:07 +0800

    add Openbrush PSP standard, write tests

--- contracts/lib.rs
+++ contracts/lib.rs
@@ -1,19 +1,27 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+#[openbrush::implementation(PSP22)]
 #[openbrush::contract]
 mod staking_contract {
-    use openbrush::contracts::psp22::*;
+    use openbrush::traits::Storage;
 
     #[ink(storage)]
+    #[derive(Default, Storage)]
     pub struct StakingContract {
-        /// Stores a single `bool` value on the storage.
+        #[storage_field]
+        psp22: psp22::Data,
         value: bool,
     }
 
+    //impl PSP22 for StakingContract {}
+
     impl StakingContract {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(initial_supply: Balance) -> Self {
+            let mut _instance = Self::default();
+            psp22::Internal::_mint_to(&mut _instance, Self::env().caller(), initial_supply)
+                .expect("Should mint");
+            _instance
         }
 
         #[ink(message)]
@@ -28,12 +36,39 @@ mod staking_contract {
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
+        use openbrush::contracts::psp22::PSP22Impl;
+        use openbrush::test_utils::*;
+        #[ink::test]
+        fn constructor_works() {
+            let accounts = accounts();
+            let mint_amount = 1000_1000_1000;
+            let staking_contract = StakingContract::new(mint_amount);
+            let alice_balc = PSP22Impl::balance_of(&staking_contract, accounts.alice);
+            assert_eq!(alice_balc, mint_amount);
+        }
+
+        #[ink::test]
+        fn transfer_works() {
+            let accounts = accounts();
+            let mint_amount = 1000_1000_1000;
+            let transfer_amount = 1_000;
+            let mut staking_contract = StakingContract::new(mint_amount);
+
+            PSP22Impl::transfer(
+                &mut staking_contract,
+                accounts.bob,
+                transfer_amount,
+                Vec::new(),
+            )
+            .expect("transfer");
+
+            let alice_balc = PSP22Impl::balance_of(&staking_contract, accounts.alice);
+            let bob_balc = PSP22Impl::balance_of(&staking_contract, accounts.bob);
+            assert_eq!(alice_balc, mint_amount - transfer_amount);
+            assert_eq!(bob_balc, transfer_amount);
+        }
     }
 }
